7a83d6623ab1a1393504872f5d6c788294520bf4
cyberchain31 <cyberchain31@gmail.com>
2023-01-01 18:52:26 +0100

    first commit

---
+++ webpage/portfolio/databasesystem/includes/footer.php
@@ -0,0 +1,15 @@
+  <!-- Footer -->
+  <footer class="py-3 bg-dark footerSize">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Patrik Lovás. All Rights Reserved 2022.</p>
+    </div>
+  </footer>
+
+<!-- Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="js/script.js"></script>
+  <!-- <script src="js/jquery-3.6.0.js"></script> -->
+
+</body>
+
+</html>
